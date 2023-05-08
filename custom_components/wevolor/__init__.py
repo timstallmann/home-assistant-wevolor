740a653407ba69c1c1d08c3836a2e18538b55a7c
Mitchell Barry <mitch.barry@gmail.com>
2023-05-08 17:55:33 -0500

    Update from deprecated method

--- custom_components/wevolor/__init__.py
+++ custom_components/wevolor/__init__.py
@@ -21,7 +21,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         host=entry.data[CONFIG_HOST]
     )
 
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 
